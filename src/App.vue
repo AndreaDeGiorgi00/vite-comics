9b2de6f2124fddea3882ca34451106bb438d607a
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-01-22 18:28:14 +0100

    header completato

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,6 @@ export default {
   <app-header></app-header>
 </template>
 
-<style >
-
+<style lang="scss">
+@use "./assets/scss/style.scss"
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,22 +1,139 @@
 <script>
 
 export default {
+    data() {
+        return {
+            bigData: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+        }
+    },
+    methods: {
+        getActive(element) {
+            if (element.current) {
+                return "active"
+            } else {
+                return "notActive"
+            }
+        },
+        makeActive(element) {
+            for (let x = 0; x < this.bigData.length; x++) {
+                if (this.bigData[x].current) {
+                    this.bigData[x].current = false;
+                }
+            }
+            element.current = true;
+        }
+    },
     name: "main header"
 }
+
 
 
 </script>
 
 <template>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, necessitatibus, animi deleniti accusantium
-        officia voluptates ipsam enim ex et excepturi consectetur. Accusamus quos ipsam alias excepturi ea nihil facere
-        tenetur.</p>
+    <section>
+
+        <div class="container">
+            <img src="img/dc-logo.png" alt="">
+
+            <ul>
+                <li v-for="i in bigData" :class="getActive(i)" :key="i.text" v-on:click="makeActive(i)">
+                    <a :class="getActive(i)" :href="i.url">{{ i.text }}</a>
+                </li>
+
+            </ul>
+
+        </div>
+    </section>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use "../assets/scss/variables" as *;
+@use "../assets/scss/partials" as *;
 
-p {
-    color: $colore_rosso;
+.container {
+    @include contenitore;
+    @include display-flex-just-sp-bt;
+    height: 90px;
+    background-color: white;
+
+    ul {
+        @include display-flex-just-sp-bt;
+        width: 66%;
+    }
+}
+
+li {
+    @include display-flex-full-center;
+    list-style-type: none;
+    height: 100%;
+    text-align: center;
+    border-bottom: 5px solid white;
+
+
+}
+
+a {
+    text-decoration: none;
+    color: black;
+    cursor: pointer;
+    font-size: 20px;
+}
+
+.active {
+    border-color: #0C7CEC;
+    color: #0C7CEC;
+
 }
 </style>
