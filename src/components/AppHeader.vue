6a3ea3963a4a554a85fe34aa7cb1aa034ebeff02
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-01-22 16:47:28 +0100

    start

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    name: "main header"
+}
+
+
+</script>
+
+<template>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, necessitatibus, animi deleniti accusantium
+        officia voluptates ipsam enim ex et excepturi consectetur. Accusamus quos ipsam alias excepturi ea nihil facere
+        tenetur.</p>
+</template>
+
+<style lang="scss">
+@use "../assets/scss/variables" as *;
+
+p {
+    color: $colore_rosso;
+}
+</style>
